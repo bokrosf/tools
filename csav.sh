a1f19cf002964ed65cf42b0d3251cc03447a73d8
bokrosf <bokros.feri@hotmail.com>
2024-08-06 12:25:53 +0200

    Basic parameter parsing.

--- csav.sh
+++ csav.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+print_error()
+{
+  echo "csav: $1"
+}
+
+help()
+{
+  # TODO Implement displaying usage.
+  echo "help"
+}
+
+case "$1" in
+  help)
+    help
+    ;;
+  "")
+    print_error "no operation specified (use 'help' for help)"
+    ;;
+  *)
+    print_error "invalid operation -- '$1'"
+    ;;
+esac
